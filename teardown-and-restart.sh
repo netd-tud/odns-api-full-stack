8a5607e4ffb23164a9163da719cbc56dd74b8d87
Patrick.B <patrick.balian@mailbox.tu-dresden.de>
2025-06-18 15:43:21 +0200

    fixed script by adding missing reference to defined resources

--- teardown-and-restart.sh
+++ teardown-and-restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker compose -f docker-compose.api.yml \
+docker compose -f docker-compose.yml -f docker-compose.api.yml \
     -f docker-compose.importer.yml down
 
 # stop db and remove volume with data
-docker compose -f docker-compose.db.yml down -v
-#docker volume rm odns-api-full-stack_postgres_data
+docker compose -f docker-compose.yml -f docker-compose.db.yml down
+docker volume rm odns-api-full-stack_postgres_data
 
 # Restart service stoped above
 docker compose -f docker-compose.yml \
