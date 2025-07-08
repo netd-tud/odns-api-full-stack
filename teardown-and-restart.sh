24d1bb75489446b2bb33f4d7b521387d8ff1ac4f
Patrick.B <patrick.balian@mailbox.tu-dresden.de>
2025-07-08 12:14:02 +0200

    stopped deleting the volume

--- teardown-and-restart.sh
+++ teardown-and-restart.sh
@@ -7,7 +7,7 @@ docker compose -f docker-compose.yml \
 
 # stop db and remove volume with data
 
-docker volume rm odns-api-full-stack_postgres_data
+#docker volume rm odns-api-full-stack_postgres_data
 
 # Restart service stoped above
 docker compose -f docker-compose.yml \
